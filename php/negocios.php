e33cbb9c0e181b29d6242e587244c9853cc43eca
Fernanda <Fernanda@Fernanda-PC>
2015-04-05 14:07:41 -0500

    prueba

--- php/negocios.php
+++ php/negocios.php
@@ -20,8 +20,50 @@ class negocios
         $this->nombres=$nombres;
         $this->edad=$edad;
         $this->arrastra=$arrastra;
-        
-            
-            
+        $this->objetoDatos=new datos('mysql:host=localhost;dbname baseproyecto','root','');
+    }
+    public function insertar()
+    {
+    try
+    {
+        $this->objetoDatos->conectar();
+        $this->objetoDatos->ejecutar("insert into estudiante (cedula,apellidos,nombres,edad,curso,arrastra) values ('$this->cedula','$this->apellidos','$this->nombres','$this->edad','$this->curso','$this->arrastra') ");
+        $this->objetoDatos->desconectar();
+    }
+   catch (PDOException $ex)
+    {
+        throw $ex;
+    }
+    }   
+
+    public function eliminar()
+    {
+      
+        $this->objetoDatos->conectar();
+        $this->objetoDatos->ejecutar("delete from estudiante where cedula=$this->cedula ");
+        $this->objetoDatos->desconectar();
+    }
+    
+    public function actualizar()
+    {
+        $this->objetoDatos->conectar();
+        $this->objetoDatos->ejecutar("update estudiante set apellidos='$this->apellidos', nombres='$this->nombres',edad='$this->edad',curso='$this->curso',arrastra='$this->arrastra' where cedula=$this->cedula");
+        $this->objetoDatos->desconectar();
+    }
+    
+    public function consultar()
+    {
+        $this->objetoDatos->conectar();
+        $rs=$this->objetoDatos->ejecutar("select* from ingresos where cedula=$this->cedula");
+        if(count($rs))
+        {
+            $this->apellidos=$rs[0]['apellidos'];
+            $this->nombres=$rs[0]['nombres'];
+            $this->edad=$rs[0]['edad'];
+            $this->curso=$rs[0]['curso'];
+            $this->arrastra=$rs[0]['arrastra'];
+        }
+        $this->objetoDatos->desconectar();
+                
     }
 }
